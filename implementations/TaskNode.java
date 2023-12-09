89a0a11658a0fe1fb09e4432ff508c07358dcc7d
t0m0h1 <104913403+t0m0h1@users.noreply.github.com>
2023-12-09 15:58:52 +0000

    TaskNode class

--- implementations/TaskNode.java
+++ implementations/TaskNode.java
@@ -4,5 +4,20 @@ public class TaskNode {
     private Task task;
     private TaskNode next;
 
-    private 
+    public TaskNode(Task task) {
+        this.task = task;
+        this.next = null;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public TaskNode getNext() {
+        return next;
+    }
+
+    public void setNext(TaskNode next) {
+        this.next = next;
+    }
 }
